6c1917f64df057666f7dba26f5a585b70b63234a
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2025-07-15 16:54:24 +0530

    UI_Basics
    1. 3 scene created
    2. 3 Buttons created to Load new scenes

---
+++ MyFirstUnityProjectOnGitHub/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public Button button;
+    public string newScene;
+    private void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+    private void OnButtonClick()
+    {
+        Debug.Log("Button Clicked");
+        SceneManager.LoadScene(newScene);
+    }
+}
